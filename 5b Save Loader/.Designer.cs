0ea1c10690d3547046a83c9807b85086d974c940
Ari Meisels <me@arimeisels.com>
2021-07-27 22:18:42 -0400

    5b Save Loader Thingamabob

---
+++ 5b Save Loader/.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace _5b_Save_Loader
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.SelectButton = new System.Windows.Forms.Button();
+            this.OpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.step1 = new System.Windows.Forms.TextBox();
+            this.step2 = new System.Windows.Forms.TextBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SavesList = new System.Windows.Forms.ListBox();
+            this.RenameButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.ImportButton = new System.Windows.Forms.Button();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.DescriptionText = new System.Windows.Forms.RichTextBox();
+            this.SaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.Location = new System.Drawing.Point(63, 12);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(91, 23);
+            this.SelectButton.TabIndex = 4;
+            this.SelectButton.Text = "Select SWF";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // step1
+            // 
+            this.step1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.step1.Location = new System.Drawing.Point(12, 17);
+            this.step1.Name = "step1";
+            this.step1.ReadOnly = true;
+            this.step1.Size = new System.Drawing.Size(45, 13);
+            this.step1.TabIndex = 5;
+            this.step1.Text = "Step 1:";
+            // 
+            // step2
+            // 
+            this.step2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.step2.Location = new System.Drawing.Point(12, 51);
+            this.step2.Name = "step2";
+            this.step2.ReadOnly = true;
+            this.step2.Size = new System.Drawing.Size(45, 13);
+            this.step2.TabIndex = 6;
+            this.step2.Text = "Step 2:";
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(63, 46);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(91, 23);
+            this.LoadButton.TabIndex = 7;
+            this.LoadButton.Text = "Load Backup";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(160, 46);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(91, 23);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save Backup";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // SavesList
+            // 
+            this.SavesList.FormattingEnabled = true;
+            this.SavesList.Location = new System.Drawing.Point(12, 88);
+            this.SavesList.Name = "SavesList";
+            this.SavesList.Size = new System.Drawing.Size(323, 147);
+            this.SavesList.TabIndex = 9;
+            this.SavesList.SelectedIndexChanged += new System.EventHandler(this.SavesList_SelectedIndexChanged);
+            // 
+            // RenameButton
+            // 
+            this.RenameButton.Location = new System.Drawing.Point(341, 88);
+            this.RenameButton.Name = "RenameButton";
+            this.RenameButton.Size = new System.Drawing.Size(91, 23);
+            this.RenameButton.TabIndex = 10;
+            this.RenameButton.Text = "Rename";
+            this.RenameButton.UseVisualStyleBackColor = true;
+            this.RenameButton.Click += new System.EventHandler(this.RenameButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(341, 117);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(91, 23);
+            this.DeleteButton.TabIndex = 11;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // ImportButton
+            // 
+            this.ImportButton.Location = new System.Drawing.Point(341, 183);
+            this.ImportButton.Name = "ImportButton";
+            this.ImportButton.Size = new System.Drawing.Size(91, 23);
+            this.ImportButton.TabIndex = 13;
+            this.ImportButton.Text = "Import";
+            this.ImportButton.UseVisualStyleBackColor = true;
+            this.ImportButton.Click += new System.EventHandler(this.ImportButton_Click);
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(341, 212);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(91, 23);
+            this.ExportButton.TabIndex = 14;
+            this.ExportButton.Text = "Export";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
+            // DescriptionText
+            // 
+            this.DescriptionText.Location = new System.Drawing.Point(259, 12);
+            this.DescriptionText.Name = "DescriptionText";
+            this.DescriptionText.Size = new System.Drawing.Size(173, 70);
+            this.DescriptionText.TabIndex = 15;
+            this.DescriptionText.Text = "";
+            this.DescriptionText.TextChanged += new System.EventHandler(this.DescriptionText_TextChanged);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 251);
+            this.Controls.Add(this.DescriptionText);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.ImportButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.RenameButton);
+            this.Controls.Add(this.SavesList);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.step2);
+            this.Controls.Add(this.SelectButton);
+            this.Controls.Add(this.step1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "5b Save Loader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.OpenFileDialog OpenFile;
+        private System.Windows.Forms.TextBox step1;
+        private System.Windows.Forms.TextBox step2;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.ListBox SavesList;
+        private System.Windows.Forms.Button RenameButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button ImportButton;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.RichTextBox DescriptionText;
+        private System.Windows.Forms.SaveFileDialog SaveFile;
+    }
+}
+
---
+++ 5b Save Loader/MainWindow.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _5b_Save_Loader
+{
+    public partial class MainWindow : Form
+    {
+        string FilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            step2.Hide();
+            ToggleButtons(0);
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            step2.Hide();
+            ToggleButtons(0);
+
+            FilePath = null;
+
+            OpenFile.FileName = "5b.swf";
+            OpenFile.Filter = "Adobe Flash movie (*.swf)|*.swf";
+            OpenFile.Title = "Upload Process (1/6)";
+
+            if (OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = Environment.GetEnvironmentVariable("AppData") + "\\Macromedia\\Flash Player\\#SharedObjects\\VSAWG93S\\localhost\\" + OpenFile.FileName.Remove(0, 3);
+
+                if (!File.Exists(Path.Combine(FilePath, "bfdia5b.sol")))
+                {
+                    MessageBox.Show("Please start level 1 for this to work!");
+                    return;
+                }
+
+                step2.Show();
+                ToggleButtons(1);
+
+                RefreshSaves();
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            if (SavesList.SelectedIndex == -1) return;
+
+            if (MessageBox.Show("Are you sure you want to replace your current save with this one?", "Are you sure?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                string[] Saves = Directory.GetDirectories(FilePath);
+                File.Copy(Path.Combine(Saves[SavesList.SelectedIndex], "bfdia5b.sol"), Path.Combine(FilePath, "bfdia5b.sol"), true);
+
+                MessageBox.Show(Saves[SavesList.SelectedIndex] + " has been set as the current save!");
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            MakeBackup();
+
+            SaveButton.Enabled = false;
+            Thread.Sleep(1000);
+            SaveButton.Enabled = true;
+
+            RefreshSaves();
+        }
+
+        private void RenameButton_Click(object sender, EventArgs e)
+        {
+            if (SavesList.SelectedIndex == -1) return;
+
+            string[] Saves = Directory.GetDirectories(FilePath);
+
+            var Save = Interaction.InputBox("Save File Name", "Save Game", GetTime(), 100, 100);
+            if (Save == "") return;
+
+            if (File.Exists(Path.Combine(FilePath, Save, "bfdia5b.sol")))
+            {
+                if (MessageBox.Show("A save by this name already exists. Do you want to overwrite it?", "Warning!", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                Directory.Move(Path.Combine(FilePath, Saves[SavesList.SelectedIndex]), Path.Combine(FilePath, Save));
+            }
+
+            ToggleButtons(2);
+
+            RefreshSaves();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (SavesList.SelectedIndex == -1) return;
+
+            string[] Saves = Directory.GetDirectories(FilePath);
+
+            if (MessageBox.Show("Are you sure you want to PERMANENTLY delete this save?", "Are you sure?", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                return;
+            }
+
+            MessageBox.Show(Saves[SavesList.SelectedIndex].Split('\\')[Saves[SavesList.SelectedIndex].Split('\\').Length - 1] + " has been deleted!");
+
+            Directory.Delete(Path.Combine(FilePath, Saves[SavesList.SelectedIndex]), true);
+
+            ToggleButtons(2);
+
+            RefreshSaves();
+        }
+
+        private void ImportButton_Click(object sender, EventArgs e)
+        {
+            OpenFile.FileName = "bfdia5b.sol";
+            OpenFile.Filter = "Shared Object Local Files (*.sol)|*.sol";
+            OpenFile.Title = "Import Save";
+
+            if (OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                var Save = Interaction.InputBox("Save File Name", "Save Game", GetTime(), 100, 100);
+                if (Save == "") return;
+
+                if (File.Exists(Path.Combine(FilePath, Save, "bfdia5b.sol")))
+                {
+                    if (MessageBox.Show("A save by this name already exists. Do you want to overwrite it?", "Warning!", MessageBoxButtons.YesNo) == DialogResult.No)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    Directory.CreateDirectory(Path.Combine(FilePath, Save));
+                }
+
+                File.Copy(OpenFile.FileName, Path.Combine(FilePath, Save, "bfdia5b.sol"), true);
+
+                File.WriteAllText(Path.Combine(FilePath, Save, "desc.txt"), "Description...");
+
+                ImportButton.Enabled = false;
+                Thread.Sleep(1000);
+                ImportButton.Enabled = true;
+
+                RefreshSaves();
+            }
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+            if (SavesList.SelectedIndex == -1) return;
+
+            string[] Saves = Directory.GetDirectories(FilePath);
+
+            SaveFile.DefaultExt = "sol";
+            SaveFile.FileName = "bfdia5b.sol";
+            SaveFile.Filter = "Shared Object Local Files (*.sol)|*.sol";
+            SaveFile.Title = "Export Save";
+
+            SaveFile.ShowDialog();
+
+            if (SaveFile.FileName == "") return;
+
+            File.Copy(Path.Combine(Saves[SavesList.SelectedIndex], "bfdia5b.sol"), SaveFile.FileName, true);
+        }
+
+        private string GetTime()
+        {
+            Regex DateRegex = new Regex(@"\b[1-9][1-2]?\/[1-3]?[1-9]\/\d*\b",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Regex TimeRegex = new Regex(@"\b[0-2]?[0-9](:[0-5][0-9]){2}\b",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            var DateString = DateTime.Now.ToString();
+
+            var Date = DateRegex.Matches(DateString)[0].Groups[0].ToString();
+            var Time = TimeRegex.Matches(DateString)[0].Groups[0].ToString();
+            var ampm = DateString.Remove(0, DateString.Length - 2);
+
+            return Date.Replace("/", "-") + "_" + Time.Replace(":", "-") + "_" + ampm;
+        }
+
+        private void MakeBackup()
+        {
+            var Save = Interaction.InputBox("Save File Name", "Save Game", GetTime(), 100, 100);
+            if (Save == "") return;
+
+            if (File.Exists(Path.Combine(FilePath, Save, "bfdia5b.sol")))
+            {
+                if (MessageBox.Show("A save by this name already exists. Do you want to overwrite it?", "Warning!", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    return;
+                }
+            } else
+            {
+                Directory.CreateDirectory(Path.Combine(FilePath, Save));
+            }
+
+            File.Copy(Path.Combine(FilePath, "bfdia5b.sol"), Path.Combine(FilePath, Save, "bfdia5b.sol"), true);
+
+            File.WriteAllText(Path.Combine(FilePath, Save, "desc.txt"), "Description...");
+
+            ToggleButtons(2);
+        }
+
+        private string ConvertTimestamp(string Timestamp)
+        {
+            Regex TimestampRegex = new Regex(@"\b[1-9][1-2]?-[1-3]?[1-9]-\d*_[0-2]?[0-9](-[0-5][0-9]){2}_(AM|PM)\b",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (TimestampRegex.Matches(Timestamp).Count == 0) return Timestamp;
+
+            return Timestamp.Split('_')[0].Replace("-", "/") + " " + Timestamp.Split('_')[1].Replace("-", ":") + " " + Timestamp.Split('_')[2];
+        }
+
+        private void DescriptionText_TextChanged(object sender, EventArgs e)
+        {
+            if (SavesList.SelectedIndex == -1) return;
+
+            string[] Saves = Directory.GetDirectories(FilePath);
+            File.WriteAllText(Path.Combine(Saves[SavesList.SelectedIndex], "desc.txt"), DescriptionText.Text);
+        }
+
+        private void SavesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SavesList.SelectedIndex == -1) return;
+
+            ToggleButtons(3);
+
+            string[] Saves = Directory.GetDirectories(FilePath);
+            var Description = File.ReadAllText(Path.Combine(Saves[SavesList.SelectedIndex], "desc.txt"));
+            
+            DescriptionText.Text = File.ReadAllText(Path.Combine(Saves[SavesList.SelectedIndex], "desc.txt"));
+        }
+
+        private void RefreshSaves()
+        {
+            SavesList.Items.Clear();
+
+            string[] Saves = Directory.GetDirectories(FilePath);
+            for (int i = 0; i < Saves.Length; i++)
+            {
+                SavesList.Items.Add(ConvertTimestamp(Saves[i].Split('\\')[Saves[i].Split('\\').Length - 1]));
+            }
+
+            ToggleButtons(2);
+        }
+
+        private void ToggleButtons(int Reason)
+        {
+            if (Reason == 0) // No file chosen
+            {
+                LoadButton.Hide();
+                SaveButton.Hide();
+                SavesList.Hide();
+                DescriptionText.Hide();
+                LoadButton.Hide();
+                RenameButton.Hide();
+                DeleteButton.Hide();
+                ImportButton.Hide();
+                ExportButton.Hide();
+            }
+
+            if (Reason == 1) // File chosen
+            {
+                LoadButton.Show();
+                SaveButton.Show();
+                SavesList.Show();
+                DescriptionText.Show();
+                LoadButton.Show();
+                RenameButton.Show();
+                DeleteButton.Show();
+                ImportButton.Show();
+                ExportButton.Show();
+            }
+
+            if (Reason == 2) // Nothing selected
+            {
+                LoadButton.Enabled = false;
+                RenameButton.Enabled = false;
+                DeleteButton.Enabled = false;
+                ExportButton.Enabled = false;
+                DescriptionText.Enabled = false;
+            }
+
+            if (Reason == 3) // Something selected
+            {
+                LoadButton.Enabled = true;
+                RenameButton.Enabled = true;
+                DeleteButton.Enabled = true;
+                ExportButton.Enabled = true;
+                DescriptionText.Enabled = true;
+            }
+        }
+    }
+}
